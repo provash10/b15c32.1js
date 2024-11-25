4398221ec6434cf85e0a57c9c0b438b2370a12db
provash10 <provash20cb@gmail.com>
2024-11-26 01:34:41 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,89 @@
+let marks = [
+    [34, 65, 89,98, 32],
+    [56, 78, 88, 67, 99],
+    [43, 67, 89, 78, 9],
+];
+
+// console.log(marks[0][0]);
+// console.log(marks[0][1]);
+// console.log(marks[0][2]);
+// console.log(marks[0][3]);
+// console.log(marks[0][4]);
+
+// console.log(marks[1][0]);
+// console.log(marks[1][1]);
+// console.log(marks[1][2]);
+// console.log(marks[1][3]);
+// console.log(marks[1][4]);
+
+// console.log(marks[2][0]);
+// console.log(marks[2][1]);
+// console.log(marks[2][2]);
+// console.log(marks[2][3]);
+// console.log(marks[2][4]);
+
+// =======================
+// row * column => 3*5 = 15
+// for (i=0; i<=2; i++){ //i = 0, 1, 2
+//     for(j=0; j<=4; j++){ //j = 0, 1, 2, 3, 4 | 0, 1, 2, 3, 4 | 0, 1, 2, 3, 4 |
+//         // console.log(marks[0][0]);
+//         console.log(marks[i][j]);
+//     }
+// }
+
+// Showing for marks and Grade
+// for (i=0; i<=2; i++){ //i = 0, 1, 2
+//     for(j=0; j<=4; j++){ //j = 0, 1, 2, 3, 4 | 0, 1, 2, 3, 4 | 0, 1, 2, 3, 4 |
+//         // console.log(marks[0][0]);
+//         console.log(marks[i][j]);
+
+//         if(marks[i][j] >= 33){
+//             console.log("Passed");
+//         }
+//         else{
+//             console.log("Failed");
+//         }
+//     }
+// }
+
+
+
+// Again katenition adding
+
+// for (i=0; i<=2; i++){ //i = 0, 1, 2
+//     for(j=0; j<=4; j++){ //j = 0, 1, 2, 3, 4 | 0, 1, 2, 3, 4 | 0, 1, 2, 3, 4 |
+//         // console.log(marks[0][0]);
+//         console.log(marks[i][j]);
+
+//         if(marks[i][j] >= 33){
+//             console.log(marks[i][j] + " = Passed");
+//         }
+//         else{
+//             console.log(marks[i][j] + " = Failed");
+//         }
+//     }
+// }
+
+// 
+// ========================
+
+// Function Idea 
+
+// function functionName()
+// {
+//     //body
+// }
+
+// call function 
+
+// functionName ();
+
+function sum(){
+    let x = 10;
+    let y =30;
+    let result = x+y;
+    console.log(result);
+}
+sum();
+
+
